2a172421958a4cd64d24980deb52764491eef826
seyiadel <seyiadel03@gmail.com>
2023-01-02 22:40:30 +0100

    add corsheaders and updated requirements.txt file

--- distrobackend/distrobackend/settings.py
+++ distrobackend/distrobackend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox',
     'drf_yasg',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = "distroapp.DistroUser"
@@ -65,6 +66,7 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -73,6 +75,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "distrobackend.urls"
 
